2a9bf2630e2468885a0d0663a805cc84c177f412
kanhaiya lal bohra <bohra0999@gmail.com>
2025-07-28 13:39:42 +0530

    feat: add CSRF token handling and improve API history error handling

--- zproject/urls.py
+++ zproject/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -8,3 +10,7 @@ urlpatterns = [
     path('', include('oxapaycallback.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
+
+# Serve static files in production
+if not settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
